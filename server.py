3cff1982712edce87b09801a4eb642f6a6560876
maciek367 <maciek2baba2@gmail.com>
2023-07-08 17:06:06 +0200

    setup working server serving data as json, add arduino code to the repo

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import serial
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    arduino = serial.Serial('/dev/ttyUSB0', 9600)
+    arduino_data = arduino.readline()
+    data = arduino_data.decode('utf-8')
+    list = data.split(' ')
+    temp = list[0]
+    hum = list[1]
+    json = '{"temp":' + temp + ',"hum":' + hum + '}'
+    return json
+
+
+if __name__ == "__main__":
+    app.run(port=4000)
